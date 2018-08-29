48192c988492ae19758881c4bac4d11eec2c7150
adeltahenti <adel.tahenti@citycable.ch>
2018-08-29 08:29:16 +0200

    README file edited, some changes in app.js

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Ajax Sandbox
+
+## Using XMLHttpRequest Object
+
+### readyState values
+
+- 0: request not initialized
+- 1: server connection established
+- 2: request received
+- 3: processing request
+- 4: request finished and response is ready
+
+### HTTP Status
+
+- 200: "OK"
+- 403: "Forbidden"
+- 404: "Not Found"
--- js/app.js
+++ js/app.js
@@ -5,19 +5,50 @@ document.getElementById('button').addEventListener('click', loadData);
 function loadData() {
   // Create XHR Object
   const xhr = new XMLHttpRequest(); // 1er
+  // console.log(`READYSTATE: ${xhr.readyState}`);
 
   // Open
   xhr.open('GET', '../data.txt', true); // 2ème
 
+  // console.log(`READYSTATE: ${xhr.readyState}`);
+
+  // Optionnal - Used for spinners/loaders
+  xhr.onprogress = function () {
+    console.log(`READYSTATE: ${xhr.readyState}`);
+  }
+
   // On load
-  xhr.onload = function () { // 3ème
+  xhr.onload = function () { // 3ème latest
+    console.log(`READYSTATE: ${xhr.readyState}`);
     if (this.status === 200) {
-      console.log(this.responseText);
+      // console.log(this.responseText);
+      document.getElementById('output').innerHTML = `
+        <h3>${this.responseText}</h3>
+      `;
     }
+  }
+  // same thing with 3ème
+  // xhr.onreadystatechange = function () {
+  //   // console.log(`READYSTATE: ${xhr.readyState}`);
+  //   if (this.status === 200 && this.readyState === 4) {
+  //     console.log(this.responseText);
+  //   }
+  // }
+
+  // Error
+  xhr.onerror = function () {
+    console.log('Request error...');
   }
 
   // Send
   xhr.send(); // 4ème
+
+  // readyState values
+  // 0: request not initialized
+  // 1: server connection established
+  // 2: request received
+  // 3: processing request
+  // 4: request finished and response is ready
 
   // HTTP Status
   // 200: "OK"

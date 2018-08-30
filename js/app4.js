ec90d010533793b538efbf075a1e7da8f3a26668
adeltahenti <adel.tahenti@citycable.ch>
2018-08-30 07:10:00 +0200

    ES6 Promises -> app4.js

--- js/app4.js
+++ js/app4.js
@@ -49,7 +49,7 @@ getPosts(); */
 
 /**** Asynchronous way ****/
 
-function createPost(post, callback) {
+/* function createPost(post, callback) {
   setTimeout(() => {
     posts.push(post);
     callback();
@@ -79,3 +79,52 @@ createPost({
   title: 'Post Four',
   body: 'This is post four'
 }, getPosts);
+*/
+
+/**** ES6 Promises ****/
+
+function createPost(post) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      posts.push(post);
+
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject('Something went wrong');
+      }
+
+    }, 2000);
+  });
+
+}
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `
+        <ul>
+          <li><strong>Title: </strong>${post.title}</li>
+          <li><strong>Title: </strong>${post.body}</li>
+        </ul>
+      `;
+      if (index !== (posts.length - 1)) {
+        output += '<hr>';
+      }
+    });
+
+    document.getElementById('result').innerHTML = output;
+  }, 1000);
+}
+
+createPost({
+  title: 'Post Four',
+  body: 'This is post four'
+}).then(function () {
+  getPosts()
+}).catch(function (err) {
+  console.log(err);
+});
